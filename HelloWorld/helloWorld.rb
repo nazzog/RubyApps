04ccdfae4e41613c9bdc870d5ff597d5b78341f2
nazzog <tialvesm@gmail.com>
2017-04-18 19:49:51 -0300

    Adicionado o primeiro programa!

---
+++ HelloWorld/helloWorld.rb
@@ -0,0 +1,5 @@
+#Meu primeiro programa em Ruby ^^
+name = "Tiago A. Mohnschmidt"
+course = "Sistemas de Informação"
+
+puts "Hello World! Meu nome é #{name}, acadêmico de #{course} e este é meu primeiro programa na linguagem Ruby."
